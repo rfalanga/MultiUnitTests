731ae6cf8568ac9b569b27e1535f4b122360c5bc
Rod Falanga <rfalanga@users.noreply.github.com>
2025-07-11 17:12:25 -0600

    Added a class to test these ideas

---
+++ MultiUnitTests/SimpleDLL/AddSomeIntegers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * This class is to provide 3 methods, so we can test using xUnit, nUnit, and MSTest.
+ */
+
+namespace SimpleDLL;
+
+internal class AddSomeIntegers
+{
+    public int AddTwo(int a)
+    {
+        return a + 2;
+    }
+
+    public int AddThree(int a)
+    {
+        return a + 3;
+    }
+
+    public int AddFour(int a)
+    {
+        return a + 4;
+    }
+}
