d08991eedc9b8bfb766bdd15b30a3699f1a08b3d
Rod Falanga <rfalanga@users.noreply.github.com>
2025-07-12 07:02:07 -0600

    Adding an xUnit testing project

---
+++ MultiUnitTests/xUnit.Test/TestingWithXUnit.cs
@@ -0,0 +1,14 @@
+﻿namespace xUnit.Test
+{
+    public class TestingWithXUnit
+    {
+        [Fact]
+        public void TestAddingTwo()
+        {             
+            int a = 1;
+            int b = 2;
+            int result = a + b;
+            Assert.Equal(3, result);
+        }
+    }
+}
