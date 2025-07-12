240bc45f41e54b62b7a408cb41d042e229f03e91
Rod Falanga <rfalanga@users.noreply.github.com>
2025-07-12 08:52:33 -0600

    Beginning adding an NUnit test project
    not fully configured.

---
+++ MultiUnitTests/NUnit3.Test/TestingWithNUnit.cs
@@ -0,0 +1,16 @@
+﻿namespace NUnit3.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
